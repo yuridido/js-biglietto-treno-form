a3dc4b8fe4d01858289795fe9448d47c64a57da6
yuridido <yuridido@yahoo.it>
2020-07-16 15:03:30 +0200

    inizio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Ciao ragazzi, esercizio di oggi: calcolo del prezzo del biglietto del treno con form <br>
+// cartella/repo **js-biglietto-treno-form**
+// Partendo dall'esercizio della scorsa settimana aggiungiamo la parte visiva come da screenshot
+// Creiamo un finto biglietto del treno con:
+// Nome passeggero
+// Codice treno (numero casuale tra 90000 e 100000 escluso)
+// Numero carrozza
+// Prezzo calcolato
+// Categoria selezionata dall'utente
+// Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
+// Buon lavoro e buon viaggio! (edited) 
